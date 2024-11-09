84215b73e7b2111f71e247c476b006865f8d264a
Nicat <86352979+Niko0802@users.noreply.github.com>
2024-11-09 16:11:48 +0400

    Create Tool.sh

---
+++ Tool.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+sudo apt-get install hydra -y
+sudo snap install metasploit-framework 
+sudo apt-get install nmap -y
+sudo apt-get install hashcat -y 
+sudo apt install dirsearch -y
+sudo apt install gobuster -y
+sudo snap install searchsploit
+sudo apt install snort -y
+sudo apt  install golang-go -y
+sudo go install -v github.com/projectdiscovery/httpx/cmd/httpx@latest
+sudo CGO_ENABLED=1 go install github.com/projectdiscovery/katana/cmd/katana@latest
+sudo go install -v github.com/projectdiscovery/nuclei/v3/cmd/nuclei@latest
+sudo go install -v github.com/projectdiscovery/naabu/cmd/naabu@latest
+sudo sudo apt install virtualbox-qt -y
+sudo sudo apt install hashid -y
+sudo sudo apt  install sqlmap -y
